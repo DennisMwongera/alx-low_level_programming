3c80913f0d7cadae28bddef5bbf31dc5346e64d3
DennisMwongera <mwendajunior95@gmail.com>
2022-10-19 06:21:38 -0700

    Functions and nested loops on low level programming

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ * _isalpha - Check Main
+ * @c: An input character
+ * Description: function returning 1 function to print
+ * Return: 1 if lowercase and upprtcase and 0 if a letter.
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int ischar = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++
+		{
+			if (c == lower || c == upper)
+				ischar = 1;
+		}
+	}
+	return (ischar);
+}
